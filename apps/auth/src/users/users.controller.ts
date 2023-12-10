e3d0483ec70bc40d7db3cd7d9da90bc51c1c5a91
infinitecoder390 <infinitecoder390@gmail.com>
2023-12-10 12:36:49 +0530

    commit

--- apps/auth/src/users/users.controller.ts
+++ apps/auth/src/users/users.controller.ts
@@ -10,7 +10,6 @@ export class UsersController {
   constructor(private readonly usersService: UsersService) {}
   @Post()
   async createUser(@Body() createUserDto: CreateUserDto) {
-    console.log("createUserDto",createUserDto)
     return this.usersService.create(createUserDto);
   }
   @Get()
